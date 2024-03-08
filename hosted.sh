f2ebf4daf1c6697db4a5421fe9e994627a5acc7d
Hosted Biz <38610254+oyin25@users.noreply.github.com>
2024-03-08 12:22:21 +0100

    Update hosted.sh

--- hosted.sh
+++ hosted.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # hosted.sh
 
-ssh -R $3:$1:localhost:$2 serveo.net
+if [[ -n $3 ]]; then
+  # If the user provides a subdomain (third argument), use it
+  ssh -R $3:$1:localhost:$2 serveo.net
+else
+  # If no subdomain is provided, omit the subdomain part
+  ssh -R $1:localhost:$2 serveo.net
+fi
